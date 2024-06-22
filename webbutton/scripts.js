43f59f15dbe2f071d797a40af18b07899ff1055b
osoammN <pattharawat.a@gmail.com>
2024-06-23 02:11:52 +0700

    l

--- webbutton/scripts.js
+++ webbutton/scripts.js
@@ -3,19 +3,20 @@ function clickCounter() {
     if (typeof (Storage) !== "undefined") {
         if (localStorage.clickcount) {
             localStorage.clickcount = Number(localStorage.clickcount) + 1;
-        }else{
-            localStorage.clickcount = 1; 
+        } 
+        if (localStorage.clickcount == 10) {
+            clicksetzero()
         }
-        document.getElementById("result").innerHTML = "Number of clicks: " + localStorage.clickcount;
-    }
-}
-function clicksetzero() {
-    if (typeof (Storage) !== "undefined") {
-        if (localStorage.clickcount) {
-            localStorage.clickcount = 1;
-        }else{
-            localStorage.clickcount = 1; 
+
+            document.getElementById("result").innerHTML = "Number of clicks: " + localStorage.clickcount;
         }
-        document.getElementById("result").innerHTML = "Number of clicks: " + localStorage.clickcount;
     }
+    function clicksetzero() {
+        if (typeof (Storage) !== "undefined") {
+            if (localStorage.clickcount) {
+                localStorage.clickcount = 1;
+            }
+            document.getElementById("result").innerHTML = "Number of clicks: " + localStorage.clickcount;
+        }
+    
 }
